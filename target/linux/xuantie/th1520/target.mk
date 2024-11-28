3f74988421a5b134b47e3eb4d9ffc948c130a18e
Estela ad Astra <i@estela.moe>
2024-11-28 01:56:03 +0100

    organise code for th1520 - link legacy dtb filename for licheepi4a - disable useless kernel config for routers, which make errors. - update config file to avoid dialogue - add wifi driver and boot firmware to default packages - fix kernel patching issue
    Signed-off-by: Estela ad Astra <i@estela.moe>

--- target/linux/xuantie/th1520/target.mk
+++ target/linux/xuantie/th1520/target.mk
@@ -3,3 +3,5 @@ include $(TOPDIR)/rules.mk
 BOARDNAME := Generic TH1520
 DTSDIR := thead
 DTSNAMES := th1520-lichee-pi-4a th1520-lichee-pi-4a-16g
+
+DEFAULT_PACKAGES += kmod-rtw88-8723ds th1520-boot-firmware
